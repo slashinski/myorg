9c431eeaa4e8a5c00bd2d0ba515d79b3f62b5a3e
Stephen Lashinski <slashinski@bpm105.local>
2019-03-23 22:33:22 -0500

    remove .js files and fix unit test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  testMatch: ['**/+(*.)+(spec|test).+(ts)?(x)'],
   transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
+    '^.+\\.(ts|html)$': 'ts-jest'
   },
   resolver: '@nrwl/builders/plugins/jest/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
